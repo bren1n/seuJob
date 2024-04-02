259a0883f89576cd3156d082be4d92a709da8651
Breno Ferreira <breno.leite@cwi.com.br>
2024-04-02 19:46:25 -0300

    First commit

---
+++ src/main/java/br/com/danluan/seuJob/SeuJobApplication.java
@@ -0,0 +1,13 @@
+package br.com.danluan.seuJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danluan/seuJob/SeuJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danluan.seuJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
